f185da79f997d02cfe915f40ae9b0ea5594b3a58
Kentaro Takiguchi <takiguchi0817@gmail.com>
2012-10-25 16:19:05 -0700

    最初のコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Betterflow::Application.config.secret_token = '8d0806b0be09b9b6429ec58aff25750d4293d79615c89f9bd74bb58fd3334ad583853d152d274e2a3cf09e0bfdd2f11bd7568896faa6c4b82aa3b869915c4ade'
