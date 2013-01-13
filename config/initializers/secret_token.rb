040e4a1fa41be758b8bc3811bba52e6b4ccd067e
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-01-13 12:06:00 +0900

    Railsのアップデートをしました(http://akasata.com/articles/288)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Betterflow::Application.config.secret_token = '8d0806b0be09b9b6429ec58aff25750d4293d79615c89f9bd74bb58fd3334ad583853d152d274e2a3cf09e0bfdd2f11bd7568896faa6c4b82aa3b869915c4ade'
+Betterflow::Application.config.secret_token = 'bef470204dde9a7f6598284664ab5424f2ab051c050a8aec0b51ba008795feb8e3eae6e3d548081485885331cad82e3b4c55bc681b806cfe06fe3deeac614e79'
